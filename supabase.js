7506ef5dbeae4e12d24d933c4345a8e5a1abce9a
seceducacaocarbonita <seceducacaocarbonita@gmail.com>
2026-03-25 14:37:49 -0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
-
-const SUPABASE_URL = 'https://jutkxuovgrccbzfmuqbc.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_Xwq-_7fBllRJEigi9eydlg_2qJ7R4lf';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
